6a04b2b8552e7e5fae34c3c6268688793110fe86
matt_kuras <mattkuras@gmail.com>
2020-07-27 11:23:55 -0400

    set up index.js file and created store

--- store-frontend/src/App.js
+++ store-frontend/src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
 
 
 class App extends React.Component {
