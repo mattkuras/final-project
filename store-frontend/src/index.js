5317519ab76a95de8d6f59d892bf8675b318ebe0
matt_kuras <mattkuras@gmail.com>
2020-07-28 17:08:26 -0400

    import dom router and wrap app component in routers

--- store-frontend/src/index.js
+++ store-frontend/src/index.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import guitarsReducer from './reducers/guitarsReducer'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 import App from './App';
 
@@ -13,7 +14,9 @@ let store = createStore(guitarsReducer, composeEnhancers(applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>
 
   ,document.getElementById('root'));
