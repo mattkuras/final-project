641110a138d2324fcab093e2f25974716903f0af
matt_kuras <mattkuras@gmail.com>
2020-07-28 13:18:56 -0400

    update reducer import

--- store-frontend/src/index.js
+++ store-frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import cartReducer from './reducers/cartReducer'
+import guitarsReducer from './reducers/guitarsReducer'
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(cartReducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(guitarsReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
