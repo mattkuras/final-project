a6f1de9e35a8c16f673358feb45a1427357a390f
matt_kuras <mattkuras@gmail.com>
2020-07-28 13:17:23 -0400

    update action creator to return fetch function with guitars data

--- store-frontend/src/actions/fetchGuitars.js
+++ store-frontend/src/actions/fetchGuitars.js
@@ -1,6 +1,12 @@
 export function fetchGuitars(action){
-    // fetch("http://localhost:3001/guitars")
-    // .then(resp => resp.json())
-    // .then(data => console.log(data[0]))
-    return action
+    return (dispatch) => {
+        fetch("http://localhost:3001/guitars")
+        .then(resp => resp.json())
+        .then(data => dispatch({
+            type: 'FETCH_GUITARS',
+            payload: data
+        }))
+        
+    }
+ 
 }
