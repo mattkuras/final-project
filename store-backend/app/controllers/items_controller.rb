d1cfdeb11185049f0a7c128f0221c42414342779
matt_kuras <mattkuras@gmail.com>
2020-07-30 14:00:50 -0400

    index action renders guitars with items

--- store-backend/app/controllers/items_controller.rb
+++ store-backend/app/controllers/items_controller.rb
@@ -2,14 +2,13 @@ class ItemsController < ApplicationController
     skip_before_action :verify_authenticity_token 
 
     def index
-        byebug
+
         @items = Item.all
-        render json: @items, except: [:updated_at, :created_at] 
+        render json: @items, except: [:updated_at, :created_at], include: [:guitar]
     end
 
     def create
         @item = Item.new(guitar_id: params[:id], cart_id: 1)
-        byebug
         if @item.save 
             render json: @item 
         else 
