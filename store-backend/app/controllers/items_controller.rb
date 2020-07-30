5a68516cd358365dc479a5bc5ff351ab64e2ec12
matt_kuras <mattkuras@gmail.com>
2020-07-30 13:41:09 -0400

    add index and create actions for item

--- store-backend/app/controllers/items_controller.rb
+++ store-backend/app/controllers/items_controller.rb
@@ -1,2 +1,29 @@
 class ItemsController < ApplicationController
+    skip_before_action :verify_authenticity_token 
+
+    def index
+        byebug
+        @items = Item.all
+        render json: @items, except: [:updated_at, :created_at] 
+    end
+
+    def create
+        @item = Item.new(guitar_id: params[:id], cart_id: 1)
+        byebug
+        if @item.save 
+            render json: @item 
+        else 
+            render json: {error: 'error saving item'}
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(
+            :cart_id,
+            :guitar_id
+        )
+    end
+
 end
