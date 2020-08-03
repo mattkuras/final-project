ddaa8dda731538fae750aa75cb8e08d91a0b9b86
matt_kuras <mattkuras@gmail.com>
2020-08-03 14:13:49 -0400

    Cart component renders cart items

--- store-frontend/src/components/Cart.js
+++ store-frontend/src/components/Cart.js
@@ -1,2 +1,27 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom'
+import {connect} from 'react-redux';
 
+import {addItem} from '../actions/addItem'
+
+class Cart extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h2>cart</h2> 
+                {this.props.cart.map(item =>
+                <li key={item.id}>{item.name}</li>)}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        guitars: state.guitars,
+        cart: state.cart
+    }
+}
+
+export default connect(mapStateToProps)(Cart)
