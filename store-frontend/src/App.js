b4819906ae115cea2b3de286340fca4e537b3247
matt_kuras <mattkuras@gmail.com>
2020-08-03 14:13:27 -0400

    renders header and cart

--- store-frontend/src/App.js
+++ store-frontend/src/App.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {fetchGuitars} from './actions/fetchGuitars'
 import GuitarsContainer from './containers/GuitarsContainer'
-
+import Cart from './components/Cart';
+import Header from "./components/Header";
+import { Route } from 'react-router-dom';
 
 class App extends React.Component {
 
-  // componentDidMount(){
-  //   this.props.fetchGuitars({type: "FETCH_GUITARS", payload: {name:'guitars'}})
-  // }
+
 
   render() {
     return (
       <div className="App">
+        <Header/>
         <GuitarsContainer/>
+         <Route exact path='/cart' component={Cart} />
       </div>
     )
   }
 } 
 
 export default connect(null, {fetchGuitars})(App);
+
