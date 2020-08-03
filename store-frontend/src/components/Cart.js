855241d11b2ce1952a147d411d67f0b2dfefce9f
matt_kuras <mattkuras@gmail.com>
2020-08-03 15:48:18 -0400

    add component did mount function that calls fetchItems

--- store-frontend/src/components/Cart.js
+++ store-frontend/src/components/Cart.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import {Route, Link} from 'react-router-dom'
 import {connect} from 'react-redux';
-
+import {fetchItems} from '../actions/fetchItems'
 import {addItem} from '../actions/addItem'
 
 class Cart extends React.Component {
+
+    componentDidMount(){
+        this.props.fetchItems()
+    }
 
     render(){
         return(
             <div>
                 <h2>cart</h2> 
                 {this.props.cart.map(item =>
-                <li key={item.id}>{item.name}</li>)}
+                <li key={item.id}>{item.name} - ${item.price}</li>)}
+                <br></br><br></br>
+                <p>{this.props.cartItems} items = ${this.props.cartPrice}</p> 
             </div>
         )
     }
@@ -20,8 +26,10 @@ class Cart extends React.Component {
 const mapStateToProps = state => {
     return {
         guitars: state.guitars,
-        cart: state.cart
+        cart: state.cart,
+        cartPrice: state.cartTotalPrice,
+        cartItems: state.cartTotalItems
     }
 }
 
-export default connect(mapStateToProps)(Cart)
+export default connect(mapStateToProps, {fetchItems})(Cart)
