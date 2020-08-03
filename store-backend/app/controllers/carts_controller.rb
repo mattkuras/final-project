c69476493185ebae3abe9b52c89a9832cb7908b2
matt_kuras <mattkuras@gmail.com>
2020-08-03 15:47:30 -0400

    render items with cart"

--- store-backend/app/controllers/carts_controller.rb
+++ store-backend/app/controllers/carts_controller.rb
@@ -2,7 +2,12 @@ class CartsController < ApplicationController
     
     def index 
         @carts = Cart.all 
-        render json: @carts, include: [:guitars]
+        render json: @carts, include: [:items]
+    end
+
+    def show
+        @cart = Cart.all.find_by(id: params[:id])
+        render json: @cart, include: [:items] 
     end
 
 end
