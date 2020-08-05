8d2d1ab4355204d64b57754859b437a50c47ff9a
matt_kuras <mattkuras@gmail.com>
2020-08-05 14:51:43 -0400

    change api endpoint to items and change payload

--- store-frontend/src/actions/fetchItems.js
+++ store-frontend/src/actions/fetchItems.js
@@ -1,11 +1,11 @@
 export function fetchItems(action){
     return (dispatch) => {
-        fetch("http://localhost:3001/carts/1")
+        fetch("http://localhost:3001/items")
         .then(resp => resp.json())
         .then(data => dispatch({
             
             type: 'FETCH_ITEMS',
-            payload: data.items
+            payload: data
 
         }))
     }
